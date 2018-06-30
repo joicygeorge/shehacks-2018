0095bf05e203a2eb5abeff8f077de24e60cf5e43
ZojaSavkovic <zojasavkovic@gmail.com>
2018-06-30 10:41:32 +1000

    put data in variable JSON

--- Graph.js
+++ Graph.js
@@ -1,61 +1,88 @@
-
-        var ctx = document.getElementById("myChart");
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-                datasets: [{
-                    label: 'Results',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(255,99,132,1)',
-                        'rgba(255,99,132,1)',
-                        'rgba(255,99,132,1)',
-                        'rgba(255,99,132,1)',
-                        'rgba(255,99,132,1)',
-                    ],
-                    borderWidth: 1
-                },{
-                    label: 'Recommended',
-                    type: 'line',
-                    data: [14, 21, 7, 11, 3, 4],
-                    radius: 0,
-                    backgroundColor: [
-                        'rgba(54, 162, 235, 0)',
-                        'rgba(54, 162, 235, 0)',
-                        'rgba(54, 162, 235, 0)',
-                        'rgba(54, 162, 235, 0)',
-                        'rgba(54, 162, 235, 0)',
-                        'rgba(54, 162, 235, 0)',
-                    ],
-                    borderColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
+var iron = {  
+   "type":"bar",
+   "data":{  
+      "labels":[  
+         "Jan",
+         "Feb",
+         "Mar",
+         "Apr",
+         "May",
+         "Jun"
+      ],
+      "datasets":[  
+         {  
+            "label":"Results",
+            "data":[  
+               12,
+               19,
+               3,
+               5,
+               2,
+               3
+            ],
+            "backgroundColor":[  
+               "rgba(255, 99, 132, 0.2)",
+               "rgba(255, 99, 132, 0.2)",
+               "rgba(255, 99, 132, 0.2)",
+               "rgba(255, 99, 132, 0.2)",
+               "rgba(255, 99, 132, 0.2)",
+               "rgba(255, 99, 132, 0.2)"
+            ],
+            "borderColor":[  
+               "rgba(255,99,132,1)",
+               "rgba(255,99,132,1)",
+               "rgba(255,99,132,1)",
+               "rgba(255,99,132,1)",
+               "rgba(255,99,132,1)",
+               "rgba(255,99,132,1)"
+            ],
+            "borderWidth":1
+         },
+         {  
+            "label":"Recommended",
+			"type": "line",
+            "radius": 0,
+            "data":[  
+               7,
+               7,
+               7,
+               7,
+               7,
+               7
+            ],
+            "backgroundColor":[  
+               "rgba(54, 162, 235, 0)",
+               "rgba(54, 162, 235, 0)",
+               "rgba(54, 162, 235, 0)",
+               "rgba(54, 162, 235, 0)",
+               "rgba(54, 162, 235, 0)",
+               "rgba(54, 162, 235, 0)"
+            ],
+            "borderColor":[  
+               "rgba(54, 162, 235, 1)",
+               "rgba(54, 162, 235, 1)",
+               "rgba(54, 162, 235, 1)",
+               "rgba(54, 162, 235, 1)",
+               "rgba(54, 162, 235, 1)",
+               "rgba(54, 162, 235, 1)"
+            ],
+            "borderWidth":1
+         }
+      ]
+   },
+   "options":{  
+      "scales":{  
+         "yAxes":[  
+            {  
+               "ticks":{  
+                  "beginAtZero":true
+               }
             }
-        });
-    
+         ]
+      }
+   }
+}
+	
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, iron);
+
